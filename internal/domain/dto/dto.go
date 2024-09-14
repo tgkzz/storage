f31b4b1c0b01910178bc34918aeb0f00f149a41f
tgkzz <lilkamavertttt@gmail.com>
2024-09-14 23:50:35 +0500

    added repo layer

--- internal/domain/dto/dto.go
+++ internal/domain/dto/dto.go
@@ -1,1 +1,50 @@
 package dto
+
+type UpdateItem struct {
+	Id       int64
+	Name     *string
+	Desc     *string
+	Quantity *int32
+	Price    *float32
+	Currency *string
+}
+
+func (u UpdateItem) HaveName() bool {
+	return u.Name != nil
+}
+
+func (u UpdateItem) SetName(str string) {
+	u.Name = &str
+}
+
+func (u UpdateItem) HaveDesc() bool {
+	return u.Desc != nil
+}
+
+func (u UpdateItem) SetDesc(str string) {
+	u.Desc = &str
+}
+
+func (u UpdateItem) HaveQuantity() bool {
+	return u.Quantity != nil
+}
+
+func (u UpdateItem) SetQuantity(q int32) {
+	u.Quantity = &q
+}
+
+func (u UpdateItem) HavePrice() bool {
+	return u.Price != nil
+}
+
+func (u UpdateItem) SetPrice(p float32) {
+	u.Price = &p
+}
+
+func (u UpdateItem) HaveCurrency() bool {
+	return u.Currency != nil
+}
+
+func (u UpdateItem) SetCurrency(s string) {
+	u.Currency = &s
+}
--- internal/service/item/item.go
+++ internal/service/item/item.go
@@ -2,12 +2,22 @@ package item
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"github.com/tgkzz/storage/internal/domain/dto"
 	"github.com/tgkzz/storage/internal/domain/models"
+	"github.com/tgkzz/storage/internal/repository/postgresql"
+	"github.com/tgkzz/storage/pkg/logger"
 	"log/slog"
 )
 
+var (
+	ErrInvalidCredentials = errors.New("invalid credentials")
+)
+
 type Item struct {
-	log *slog.Logger
+	log  *slog.Logger
+	repo postgresql.Repo
 }
 
 type ItemService interface {
@@ -17,8 +27,8 @@ type ItemService interface {
 	GetItemById(ctx context.Context, itemId int64) (models.Item, error)
 }
 
-func New() (*ItemService, error) {
-	return nil, nil
+func New(log *slog.Logger, repo postgresql.Repo) (ItemService, error) {
+	return &Item{log: log, repo: repo}, nil
 }
 
 func (i *Item) CreateItem(ctx context.Context, opts ...models.OptionFunc) error {
@@ -26,7 +36,18 @@ func (i *Item) CreateItem(ctx context.Context, opts ...models.OptionFunc) error 
 
 	item := models.NewItem(opts...)
 
-	_ = item
+	l := i.log.With(
+		slog.String("op", op),
+		slog.String("Name", item.Name),
+	)
+
+	l.Info("create item request")
+
+	if _, err := i.repo.CreateNewItem(ctx, *item); err != nil {
+		l.Error("failed to create new item", logger.Err(err))
+
+		return fmt.Errorf("%s: %w", op, err)
+	}
 
 	return nil
 }
@@ -40,6 +61,17 @@ func (i *Item) UpdateItem(ctx context.Context, itemId int64, opts ...models.Opti
 
 	for _, opt := range opts {
 		opt(item)
+	}
+
+	l := i.log.With(
+		slog.String("op", op),
+		slog.Int64("id", item.ID),
+	)
+
+	l.Info("update item request")
+
+	if err := i.repo.UpdateItemById(ctx, dto.UpdateItem{}); err != nil {
+		return fmt.Errorf("%s: %w", op, err)
 	}
 
 	return nil
